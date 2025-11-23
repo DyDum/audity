06ee2e524d76dc1321dfb288370e84dcb4e95a3c
wikkizs <clementlavallee6@gmail.com>
2025-11-23 17:42:44 +0100

    Add : rules

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo ./venv/bin/python main.py scan --rules ./rules --output ./reports_output
+sudo ./venv/bin/python main.py --verbose scan --rules ./rules --output ./reports_debug 2>&1 | tee scan_debug.log
