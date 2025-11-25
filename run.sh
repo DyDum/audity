783f228b6163842faa29d518a8bc59315634beb9
wikkizs <clementlavallee6@gmail.com>
2025-11-25 20:47:08 +0100

    add run

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+# AUdit script with debug logging
 sudo ./venv/bin/python main.py --verbose scan --rules ./rules --output ./reports_debug 2>&1 | tee scan_debug.log
+
+# AUdit script with automatic correction
+#sudo ./venv/bin/python main.py --verbose scan --rules ./rules --output ./reports_correction --fix
